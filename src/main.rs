bd097ead47ef2d6f81163564412eaa0b768d1a82
Axel Ibarrondo <axeljoaquin2202@gmail.com>
2026-07-20 20:23:37 -0300

    remove imports

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use std::{
-    env,
-    fs::File,
-    io::{self, BufRead, BufReader},
-};
-
-fn count_lines<R: BufRead>(reader: R) -> io::Result<usize> {
+fn count_lines<R: std::io::BufRead>(reader: R) -> std::io::Result<usize> {
     let mut count = 0;
     for _ in reader.lines() {
         count += 1;
@@ -12,17 +6,17 @@ fn count_lines<R: BufRead>(reader: R) -> io::Result<usize> {
     Ok(count)
 }
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = std::env::args().skip(1).collect();
 
     if args.is_empty() {
-        let stdin = io::stdin();
+        let stdin = std::io::stdin();
         let line_count = count_lines(stdin.lock())?;
         println!("{line_count}");
     } else {
         for file_path in &args {
-            let file = File::open(file_path)?;
-            let reader = BufReader::new(file);
+            let file = std::fs::File::open(file_path)?;
+            let reader = std::io::BufReader::new(file);
             let line_count = count_lines(reader)?;
             println!("{} {}", line_count, file_path);
         }
